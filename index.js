5892cf2bca9ce16f690e82b06e00e2ddb9bb4378
justinhambleton <justin@hambleton.co>
2024-07-02 11:07:56 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// my-node-app/index.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
